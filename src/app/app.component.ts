1b205bf2a191ec143cc0f31761cea09b3ad3ab06
Jeremy <Jeremy@sixsentixs-mbp.home>
2018-01-25 13:44:52 +0100

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ToDoIt';
+  version = '0.0.1';
+  
+  resolvedtodos = [
+    {
+      label: 'Implement basic functionalities',
+    }
+  ];
+  
+  todos = [
+    {
+      label: 'Finish new page layout',
+    },
+    {
+      label: 'Display error messages',
+    },
+    {
+      label: 'Add more task attributes'
+    }
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo = {
+      label: newTodoLabel
+    };
+
+    if(newTodo.label == '') {  
+      
+   } else {
+    //this.todos.push(newTodo); 
+    this.todos.unshift(newTodo);
+   }
+   
+    
+  }
+
+  resolveTodo(newTodoLabel, todo) {
+    var newTodo = {
+      label: newTodoLabel
+    };
+
+    this.resolvedtodos.unshift(newTodo);
+    this.todos.splice(todo, 1);
+  }
+  
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter( t => t.label !== todo.label);
+  }
 }
